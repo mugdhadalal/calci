32f2183a6a55a9545b2dfa660ae42046a41a3def
Mugdha Dalal <mugdhadalal@example.com>
2020-02-02 10:51:52 -0500

    commit

--- calculator.py
+++ calculator.py
@@ -3,11 +3,27 @@ import fire
 class Calculator(object):
 
   def add(self, x, y):
-    return x + y
+    return x + y 
+
+  def sub(self, x, y):
+    return x - y
+
+  def div(self, x, y):
+    return x / y
 
   def multiply(self, x, y):
     return x * y
+ 
+  def add(self, x, y):
+    return x + y 
+
+  def mod(self, x, y):
+    return x % y
+
+  def double(self, x):
+    return 2 * x
 
 if __name__ == '__main__':
   calculator = Calculator()
   fire.Fire(calculator)
+
