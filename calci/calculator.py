b0bd8517c93caa75c5600742c346a4d55a7965ff
Mugdha Dalal <mugdhadalal@example.com>
2020-02-08 07:01:38 -0500

    updated cod

--- calci/calculator.py
+++ calci/calculator.py
@@ -1,1 +1,31 @@
+import fire
+
+
+def add(x, y):
+  result = x + y
+  value = int(input( 'enter the value:' ))
+  number = result + value
+  return number
+
+def multiply(x, y):
+  result = x * y
+  value = int(input( 'enter the value:' ))
+  number = result * value
+  return number
+
+def division(x, y):
+  result = x / y
+  value = int(input( 'enter the value:' ))
+  number = result / value
+  return number
+
+def subtract(x, y):
+  result = x - y
+  value = int(input( 'enter the value:' ))
+  number = result - value
+  return number
+
+
+if __name__ == '__main__':
+ fire.Fire()
 
