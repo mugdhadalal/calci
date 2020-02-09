d9d429cf58842702e02d93dfc91aee17b3574fd8
Mugdha Dalal <mugdhadalal@example.com>
2020-02-09 10:07:49 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def read(*paths):
+    """ read files """
+    with open(os.path.join(*paths), 'r') as filename:
+        return filename.read()
 
 setup(
-    name = 'calculator_project'
+    name="calci",
+    version="0.2",
+    long_description=(read('README.rst')),
+    author="Mugdha Dalal",
+    author_email="mugdhadalal@gmail.com",
+    url=" https://github.com/mugdhadalal/calci",
+    packages=['calci'],
     entry_points={
         'console_scripts': [
-            'calci = calci:main',
+            'calci=calci:main',
         ],
-    packages = ['an_example_pypi_project', 'tests'],
-    description = ("An demonstartion of calculator orogram"),
-    platforms = ["Linux"],
-    long_description=read('README')
-    author = "Mugdha Dalal",
-    author_email = "mugdhadalal@gmail.com",
-    url = "https://github.com/mugdhadalal/calci",
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+    ]
 )
+
