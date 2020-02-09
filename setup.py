2dda9f2a5abe6b6deea74e8da7418053d695738d
Mugdha Dalal <mugdhadalal@example.com>
2020-02-09 08:10:37 -0500

    updated file

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python3
-"""Calculator project"""
-from setuptools import fire_packages, setup
 
-setup(name = 'fire',
-    version = '3.0',
-    description = "Fire module.",
-    long_description = long_description,
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'calculator_project'
+    entry_points={
+        'console_scripts': [
+            'calci = calci:main',
+        ],
+    packages = ['an_example_pypi_project', 'tests'],
+    description = ("An demonstartion of calculator orogram"),
     platforms = ["Linux"],
-    author="Mugdha Dalal",
-    author_email="mugdhadalal@gmail.com",
-    url="https://github.com/mugdhadalal/calci",
-    packages=fire_packages()
+    long_description=read('README')
+    author = "Mugdha Dalal",
+    author_email = "mugdhadalal@gmail.com",
+    url = "https://github.com/mugdhadalal/calci",
 )
