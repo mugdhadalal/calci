8628d8ba85c6b1bde810b51650569226ea251c44
Mugdha Dalal <mugdhadalal@example.com>
2020-02-10 11:08:49 -0500

    updated code

--- calci/calculator.py
+++ calci/calculator.py
@@ -1,31 +1,32 @@
 import fire
 
+class Calculator(object):
 
-def add(x, y):
-  result = x + y
-  value = int(input( 'enter the value:' ))
-  number = result + value
-  return number
+  def add(x, y):
+    result = x + y
+    value = int(input( 'enter the value:' ))
+    number = result + value
+    return number
 
-def multiply(x, y):
-  result = x * y
-  value = int(input( 'enter the value:' ))
-  number = result * value
-  return number
+  def multiply(x, y):
+    result = x * y
+    value = int(input( 'enter the value:' ))
+    number = result * value
+    return number
 
-def division(x, y):
-  result = x / y
-  value = int(input( 'enter the value:' ))
-  number = result / value
-  return number
+  def division(x, y):
+    result = x / y
+    value = int(input( 'enter the value:' ))
+    number = result / value
+    return number
 
-def subtract(x, y):
-  result = x - y
-  value = int(input( 'enter the value:' ))
-  number = result - value
-  return number
+  def subtract(x, y):
+    result = x - y
+    value = int(input( 'enter the value:' ))
+    number = result - value
+    return number
 
 
 if __name__ == '__main__':
- fire.Fire()
-
+  calculator = Calculator()
+  fire.Fire(Calculator)
