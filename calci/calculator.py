ddb355b10a4dd96826363faf164e915ab0cda393
Mugdha Dalal <mugdhadalal@example.com>
2020-02-16 09:00:52 -0500

    issue solved

--- calci/calculator.py
+++ calci/calculator.py
@@ -2,29 +2,6 @@ import fire
 
 class Calculator(object):
 
-  def add(self, x, y):
-      value = x + y
-      number = int(input("enter a no: "))
-      result = value + number
-      return result
-
-  def multiply(self, x, y):
-      value = x * y
-      number = int(input("enter a no: "))
-      result = value * number
-      return result
-
-  def div(self, x, y):
-      value = x / y
-      number = int(input("enter a no: "))
-      result = value / number
-      return result
-
-  def sub(self, x, y):
-      value = x - y
-      number = int(input("enter a no: "))
-      result = value - number
-      return result
 
   def solve(self, p):
       evaluate = p
