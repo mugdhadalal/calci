85c5545f1b7ef7caa994b647916afe96a3dd6d39
Mugdha Dalal <mugdhadalal@example.com>
2020-02-23 11:25:27 -0500

    new version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md") as readme_file:
 
 setup(
     name="pycalci",
-    version="0.0.1",
+    version="0.1.1",
     author="Mugdha Dalal",
     author_email="mugdhadalal@gmail.com",
     url="https://github.com/mugdhadalal/pycalci",
@@ -22,7 +22,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3 :: Only",
     ],
-    install_requires=['fire>=0.0.1'],
+    install_requires=['fire>=0.1.1'],
     entry_points={"console_scripts": ["pycalci=pycalci:main"]},
     long_description=readme,
     long_description_content_type="text/markdown",
